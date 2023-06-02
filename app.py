39df73beb63867479a684a567a8f1e2de5fe4ac9
poisonshade <imildositoe@gmail.com>
2023-06-02 19:14:41 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+def main():
+    text = "Python"
+    print(len(text))
+
+    concatenated = text.join(("is the best language", "in the world"))
+    concatenated = text + " is not going to die"
+    concatenated = concatenated[6:len(concatenated)]
+    concatenated = concatenated[4:]
+    concatenated = concatenated[:-4]
+    count = concatenated.count("o")
+    is_in = "not" in concatenated
+    
+    print(concatenated)
+    print(count)
+    print(is_in)
+
+if __name__ == '__main__':
+    main()
