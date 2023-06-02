8586e2d0d65bcb6d1db248d6431f15da4a0f19f7
poisonshade <imildositoe@gmail.com>
2023-06-02 20:42:46 +0200

    kwargs functions created

--- functions.py
+++ functions.py
@@ -12,5 +12,14 @@ def main():
     print("The total payment is: {}".format(payment_day(8)))
     print("The total payment is: " + str(payment_day(8) + 150))
 
+    bucket = {1: "My", 2: 1, 3: "st favorite language, that's ", 4: True, 5: 0.07}
+    multiples_parameters(value = bucket)
+    print("{}".format("-------------------Separator------------------"))
+    multiples_parameters(first_value = "Pyhon", second_value = 25, third_value = "is", forth_value = True, fifth_value = 0.007)
+
+def multiples_parameters(**kwargs):
+    for key, value in kwargs.items():
+        print("The {}: {}".format(key, value))
+
 if __name__ == '__main__':
     main()
