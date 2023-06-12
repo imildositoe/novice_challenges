2c9b84fb2cd56f7f5bba01cda4ef36f7b3ff90ca
poisonshade <imildositoe@gmail.com>
2023-06-12 21:05:30 +0200

    create file function added

--- files_io.py
+++ files_io.py
@@ -1,5 +1,6 @@
-class Information:
-    pass
+class Files:
 
-class Data:
-    pass
+    def createFile():
+        file = open("text_file.txt", "w")
+        file.write("This is the best code ever\nSure, sir!\n")
+        file.close()
