2c3a2287b8d7818bba361f1cb4634393da187403
poisonshade <imildositoe@gmail.com>
2023-07-03 00:07:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def main():
     print(len(text))
 
     concatenated = text.join(("is the best language", "in the world"))
-    concatenated = text + " is not going to die"
+    concatenated = text + " is not going to die soon"
     concatenated = concatenated[6:len(concatenated)]
     concatenated = concatenated[4:]
     concatenated = concatenated[:-4]
