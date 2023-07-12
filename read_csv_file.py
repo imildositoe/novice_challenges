819514d41564ba40a35bf0509da4bd1601d0b09a
poisonshade <imildositoe@gmail.com>
2023-07-12 07:53:29 +0200

    Update read_csv_file.py

--- read_csv_file.py
+++ read_csv_file.py
@@ -3,7 +3,6 @@ import pandas as pd
 def main():
     df_input_file = pd.read_csv(filepath_or_buffer="external_datasets/input_file_with_nan.csv")
     print(df_input_file)
-    print(df_input_file)
 
 if __name__ == '__main__':
     main()
