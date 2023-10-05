9697585a794ec03d552725946a26b5a69379ae6c
poisonshade <imildositoe@gmail.com>
2023-10-05 17:33:29 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,6 +8,7 @@ def payment_day(working_hours, payment_hour = 25):
     '''
     return working_hours * payment_hour
 
+
 def main():
     print("The total payment is: {}".format(payment_day(8)))
     print("The total payment is: " + str(payment_day(8) + 150))
@@ -16,6 +17,7 @@ def main():
     bucket = {1: "My", 2: 1, 3: "st favorite language in the world, that's ", 4: True, 5: 0.07}
     multiples_parameters(value = bucket)
     multiples_parameters(first_value = "Pyhon", second_value = 25, third_value = "is", forth_value = True, fifth_value = 0.007, sixty_value = None, seventy_value = 'data visualization')
+
 
 def multiples_parameters(**kwargs):
     for key, value in kwargs.items():
