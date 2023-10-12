a8b96d253659e4b30720f176463be8755e0ef3fd
poisonshade <imildositoe@gmail.com>
2023-10-12 21:52:43 +0200

    Update iterators_generators.py

--- iterators_generators.py
+++ iterators_generators.py
@@ -5,6 +5,7 @@ def sequence_number_generator(low, high):
         yield low
         low += 1
 
+
 def main():
     number_list = []
     for number in sequence_number_generator(0, 10):
@@ -17,6 +18,7 @@ def main():
         if (sequence == 7):
             print()
             break
+
 
 if __name__ == '__main__':
     main()
