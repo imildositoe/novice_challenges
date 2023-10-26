926e77447706915f43779bc131f2b69c543f390e
poisonshade <imildositoe@gmail.com>
2023-10-26 09:46:55 +0200

    Update iterators_generators.py

--- iterators_generators.py
+++ iterators_generators.py
@@ -9,11 +9,13 @@ def main():
     number_list = []
     for number in sequence_number_generator(0, 10):
         number_list.append(number)
+        
     print(number_list)
 
     sequence = count(start = 2.5, step = 0.5)
     while (next(sequence) <= 8):
         print(next(sequence))
+        
         if (sequence == 7):
             print()
             break
