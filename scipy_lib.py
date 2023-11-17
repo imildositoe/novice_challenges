8f02c6fa7f4a90b04c4596d7d1b71a210c788727
poisonshade <imildositoe@gmail.com>
2023-11-17 21:37:55 +0200

    Update scipy_lib.py

--- scipy_lib.py
+++ scipy_lib.py
@@ -64,12 +64,10 @@ def main():
     
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.title("Functions Interpolation Example")
-    
+    plt.title("Functons Interpolation Example")
     
     print("Visually showing the example of the function")
     plt.show()
-
 
 if __name__ == '__main__':
     main()
