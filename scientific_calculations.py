929c52601bdead2fd4ff2e9a76720ecddeac4dc3
poisonshade <imildositoe@gmail.com>
2024-01-06 19:10:17 +0200

    Update scientific_calculations.py

--- scientific_calculations.py
+++ scientific_calculations.py
@@ -10,6 +10,7 @@ def main():
     
     df_data = pd.DataFrame(data=dictionary)
     print(df_data)
+    
     try:
         # Testing next function with loop statement
         while (df_data.next() != None):
@@ -17,6 +18,7 @@ def main():
             print(df_data)
     except Exception:
         pass
+        
 
 if __name__ == '__main__':
     main()
