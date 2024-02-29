7a33dbbcf59355e5b55554399f957c400e544e8f
poisonshade <imildositoe@gmail.com>
2024-02-29 10:14:50 +0200

    Update scipy_lib.py

--- scipy_lib.py
+++ scipy_lib.py
@@ -60,13 +60,11 @@ def main():
     ax.plot(x, y_linear_interpolation, "r", label="linear interpolation")
     ax.plot(x, y_cubic_interpolation, "g", label="cubic interpolation")
     ax.legend(loc=3)
-
  
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title("Functons Interpolation Example")
 
- 
     print("Visually showing the example of the function")
     plt.show()
 
