6108327c4721f7289ccbb7fb65fd485c37702118
poisonshade <imildositoe@gmail.com>
2024-06-02 15:01:30 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -17,7 +17,6 @@ def main():
     multiples_parameters(value = bucket)
     multiples_parameters(first_value = "Pyhon", second_value = 25, third_value = "is", forth_value = True, fifth_value = 0.007, sixty_value = None, seventy_value = 'data visualization')
 
-
 def multiples_parameters(**kwargs):
     for key, value in kwargs.items():
         if value == None:
