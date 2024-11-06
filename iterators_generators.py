f429cdf0d9289c8344e702c22375c84982f0906d
poisonshade <imildositoe@gmail.com>
2024-11-06 20:39:02 +0200

    Update iterators_generators.py

--- iterators_generators.py
+++ iterators_generators.py
@@ -17,10 +17,10 @@ def main():
     while (next(sequence) <= 8):
         print(next(sequence))
 
-
         if (sequence == 7):
             print('Breaking...')
             break
+
 
 if __name__ == '__main__':
     main()
