a42634868cea1f228860e5a56cca4ce3391786b6
poisonshade <imildositoe@gmail.com>
2025-02-07 08:52:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ def main():
     text = "Python language"
     print(len(text))
 
-
     concatenated = text.join(("is the best language", "in the world"))
     concatenated = text + " is not going to die soon"
     concatenated = concatenated[6:len(concatenated)]
@@ -14,6 +13,7 @@ def main():
     print(concatenated)
     print(count)
     print(is_in)
+
 
 if __name__ == '__main__':
     main()
